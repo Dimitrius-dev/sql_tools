cc8e22473e6e129f676c7de93cccdc58ee6dca2f
Dimitrius <dima_belov_02@bk.ru>
2021-12-18 03:45:30 +0300

    v0.0.1

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <string>
+#include <iostream>
+#include <pqxx/pqxx>
+#include <list>
+
+class One{
+private:
+	int id;
+	std::string name;
+public:
+	One(int id, std::string name){
+		this->id = id;	
+		this->name = name;	
+	}
+
+
+	std::string toString(){
+		return ("id: " + std::to_string(this->id) + " name: " + this->name + "\n");
+	}
+};
+
+void select(std::string config, std::list<One> &one, std::string command){
+	one.clear();	
+
+	try
+	{
+		pqxx::connection connectionObject(config.c_str());
+		pqxx::work worker(connectionObject);
+		pqxx::result response = worker.exec(command.c_str());
+
+		for (auto row : response) {
+			one.push_back(One(row[0].as<int>(), row[1].as<std::string>()));
+	    	}
+
+	}
+
+    	catch (const std::exception& e)
+    	{
+       		std::cerr << e.what() << std::endl;
+    	}
+}
+
+void insert(std::string config, std::list<One> &one, std::string command){
+	try
+	{
+		pqxx::connection connectionObject(config.c_str());
+		pqxx::work worker(connectionObject);
+		pqxx::result response = worker.exec(command.c_str());
+
+		worker.commit();
+	}
+
+    	catch (const std::exception& e)
+    	{
+       		std::cerr << e.what() << std::endl;
+    	}
+}
+
+void update(std::string config, std::list<One> &one, std::string command){
+	try
+	{
+		pqxx::connection connectionObject(config.c_str());
+		pqxx::work worker(connectionObject);
+		pqxx::result response = worker.exec(command.c_str());
+
+
+	}
+
+    	catch (const std::exception& e)
+    	{
+       		std::cerr << e.what() << std::endl;
+    	}
+}
+
+void show(std::list<One> &one){
+	for (auto iter = one.begin(); iter != one.end(); iter++){
+        	std::cout << iter->toString();
+	}
+	std::cout<<"-----------\n";
+}
+
+int main()
+{
+	std::string connectionString = "host=localhost port=5432 dbname=maindb user=dmitrii password=dimabelov02";//ostgres password=DimaJane";
+
+	std::list<One> one;
+
+	select(connectionString, one, "SELECT * FROM one");
+
+	show(one);
+
+	std::string buf = std::string("INSERT INTO one (name) VALUES" ) + "('Vlad');";
+
+	insert(connectionString, one, buf);
+
+	show(one);
+
+	return 0;
+}
